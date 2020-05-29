7a83a1e0a62ac94a22081783609d001a3a1acdb0
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-29 19:29:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,15 +3,24 @@ require 'pry'
 def line(array)
   array.map!.with_index(1) do |name, index|
     "#{index}. #{name}"
-end 
+  end 
 
 
-if array.length > 0
-  puts "The line is currently: #{array.join(" ")}"
-else 
+  if array.length > 0
+    puts "The line is currently: #{array.join(" ")}"
+  else 
     puts "The line is currently empty."
-end
+  end
 end 
 
 
 
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end 
+
+
+def now_serving(array)
+  puts "There is nobody waiting to be served!"
+end
