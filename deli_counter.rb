8dbec640b36eccd1d23f608785a00087e75a6a06
Jacques Potgieter <potgieter.g.jacques@gmail.com>
2020-05-29 19:05:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,4 @@
-def line(array)
-  the_line = []
-  if array.length == 0
-    puts "The line is currently empty."
-  else 
-    array.map.with_index(1) do |array, index|
-    puts "The line is currently: #{index}. #{array}"
-  end
 
-end
+
+
 
